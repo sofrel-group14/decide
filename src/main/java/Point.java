517b7210e9ca2983f8cd8929664c5d7fb805cba2
K2017 <y.j.paetzelt@gmail.com>
2021-01-31 18:13:38 +0100

    (fix) Add equals override to Point. Fixes #32

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -6,4 +6,14 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+
+    @Override
+    public boolean equals(Object other){
+        if (other == null) return false;
+        if (other == this) return true;
+        if (!(other instanceof Point)) return false;
+        Point otherPoint = (Point)other;
+
+        return y == otherPoint.y && x == otherPoint.x;
+    }
 }
