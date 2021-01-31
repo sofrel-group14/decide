f9f09144d1e7cafb1769abbb7644f00fcee7b587
Axel Elmarsson <axel.elmarsson@gmail.com>
2021-01-31 17:09:23 +0100

    (feat) Implement point in circle function
    *Implements a function that checks if a point is within or on the edge
    of a circle.
    *Change Point.java to use correct indentation.
    *Related to #26

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -1,9 +1,21 @@
 public class Point {
-    public double x;
-    public double y;
+  public double x;
+  public double y;
 
-    public Point(double x, double y){
-        this.x = x;
-        this.y = y;
-    }
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  /**
+   * Function that determines if this point is contained within (or on the edge)
+   * of a circle.
+   * 
+   * @param radius the radius of the circle (inclusive).
+   * @return true if in or on the edge of the circle, false otherwise.
+   */
+  public boolean isInCircle(double radius) {
+    // Assumes perfect circle, which (I think??) is a correct assumption in the assignment.
+    return Math.pow(this.x, 2) + Math.pow(this.y, 2) <= Math.pow(radius, 2);
+  }
 }
