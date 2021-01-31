4eb1a413cfce63a8c447714185aead5616212725
K2017 <y.j.paetzelt@gmail.com>
2021-01-31 18:29:41 +0100

    (feat) Add tests for LIC6

---
+++ src/test/java/LIC6Test.java
@@ -0,0 +1,86 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public final class LIC6Test {
+  private final Parameters params = new Parameters();
+
+  @Test
+  public void LIC6IdenticalTrue() {
+    params.DIST = 5;
+    params.N_PTS = 5;
+
+    Point[] points = new Point[]{
+      new Point(0, 0),
+      new Point(1, 0),
+      new Point(0, 2),
+      new Point(3, 0),
+      new Point(0, 0),
+      new Point(1, 0),
+      new Point(0, 2),
+    };
+
+    CMV cmv = new CMV(params, points);
+    cmv.populate();
+    Assertions.assertTrue(cmv.get()[6]);
+  }
+
+  @Test
+  public void LIC6IdenticalFalse() {
+    params.DIST = 7;
+    params.N_PTS = 5;
+
+    Point[] points = new Point[]{
+      new Point(0, 0),
+      new Point(1, 0),
+      new Point(0, 2),
+      new Point(3, 0),
+      new Point(0, 0),
+      new Point(1, 0),
+      new Point(0, 2),
+    };
+
+    CMV cmv = new CMV(params, points);
+    cmv.populate();
+    Assertions.assertFalse(cmv.get()[6]);
+  }
+
+  @Test
+  public void LIC6LineTrue() {
+    params.DIST = 1;
+    params.N_PTS = 5;
+
+    Point[] points = new Point[]{
+      new Point(0, 0),
+      new Point(1, 0),
+      new Point(0, 2),
+      new Point(1, 1),
+      new Point(5, 0),
+      new Point(6, 7),
+      new Point(0, 0),
+    };
+
+    CMV cmv = new CMV(params, points);
+    cmv.populate();
+    Assertions.assertTrue(cmv.get()[6]);
+  }
+
+  @Test
+  public void LIC6LineFalse() {
+    params.DIST = 20;
+    params.N_PTS = 5;
+
+    Point[] points = new Point[]{
+      new Point(0, 0),
+      new Point(1, 0),
+      new Point(0, 2),
+      new Point(1, 1),
+      new Point(5, 0),
+      new Point(6, 7),
+      new Point(0, 0),
+    };
+
+    CMV cmv = new CMV(params, points);
+    cmv.populate();
+    Assertions.assertFalse(cmv.get()[6]);
+  }
+}
