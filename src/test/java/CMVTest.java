cca29b14b7d8e9a350bc52c03d3c705acbbee5e6
Axel Elmarsson <axel.elmarsson@gmail.com>
2021-01-31 17:39:46 +0100

    (misc) Auto format files

--- src/test/java/CMVTest.java
+++ src/test/java/CMVTest.java
@@ -1,46 +1,36 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-public class CMVTest{
+
+public class CMVTest {
   @Test
   /**
-  * @Test gives True Boolean Value based on Test  
-  * It Test that it can find 3 points in three different quadrants
-  * It Test that it can skips the first data points to find the next 3 consecitive data points
-  **/
-  public void TestLIC4True(){
+   * @Test gives True Boolean Value based on Test It Test that it can find 3
+   *       points in three different quadrants It Test that it can skips the first
+   *       data points to find the next 3 consecitive data points
+   **/
+  public void TestLIC4True() {
 
     Parameters params = new Parameters();
-        
+
     params.Q_PTS = 3;
     params.QUADS = 2;
 
-    Point[] dp = new Point[]{
-        new Point(1,1),
-        new Point(1,2),
-        new Point(-1,1),
-        new Point(-1,-1)
-      };
+    Point[] dp = new Point[] { new Point(1, 1), new Point(1, 2), new Point(-1, 1), new Point(-1, -1) };
 
     CMV cmv = new CMV(params, dp);
     cmv.populate();
     assertTrue(cmv.get()[4] == true);
-  
-	}
 
+  }
 
-    public void TestLIC4False(){
+  public void TestLIC4False() {
 
     Parameters params = new Parameters();
-        
+
     params.Q_PTS = 3;
     params.QUADS = 2;
 
-    Point[] dp = new Point[]{
-        new Point(1,1),
-        new Point(1,1),
-        new Point(1,1),
-        new Point(1,1)
-    };
+    Point[] dp = new Point[] { new Point(1, 1), new Point(1, 1), new Point(1, 1), new Point(1, 1) };
 
     CMV cmv = new CMV(params, dp);
     cmv.populate();
