8695deef867b2695765904a3e1c3dacc8513ad3d
K2017 <y.j.paetzelt@gmail.com>
2021-01-31 18:15:49 +0100

    (fix) Indentation in Point.java

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -1,19 +1,19 @@
 public class Point {
-    public double x;
-    public double y;
+  public double x;
+  public double y;
 
-    public Point(double x, double y){
-        this.x = x;
-        this.y = y;
-    }
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
 
-    @Override
-    public boolean equals(Object other){
-        if (other == null) return false;
-        if (other == this) return true;
-        if (!(other instanceof Point)) return false;
-        Point otherPoint = (Point)other;
+  @Override
+  public boolean equals(Object other) {
+    if (other == null) return false;
+    if (other == this) return true;
+    if (!(other instanceof Point)) return false;
+    Point otherPoint = (Point) other;
 
-        return y == otherPoint.y && x == otherPoint.x;
-    }
+    return y == otherPoint.y && x == otherPoint.x;
+  }
 }
