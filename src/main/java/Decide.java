297d219ac808ea1fc41bb2e73ebe998c1a8c7497
K2017 <y.j.paetzelt@gmail.com>
2021-02-03 07:36:19 +0100

    (format) Fix indentation

--- src/main/java/Decide.java
+++ src/main/java/Decide.java
@@ -1,37 +1,38 @@
 public class Decide {
 
-    Parameters parameters;
-    boolean[] puv = new boolean[15];
-    CMV cmv;
-    boolean[][] pum = new boolean[15][15];
-    boolean[] fuv = new boolean[15];
-    LCM[][] lcm;
+  Parameters parameters;
+  boolean[] puv = new boolean[15];
+  CMV cmv;
+  boolean[][] pum = new boolean[15][15];
+  boolean[] fuv = new boolean[15];
+  LCM[][] lcm;
 
-    /**
-     * @param parameters, LCM, PUV and datapoints.
-     * creates all necessary variables that is needed to check if a launch can be made
-     */
-    public Decide(Parameters parameters, LCM[][] lcm, boolean[] puv, Point[] points) {
+  /**
+   * @param parameters, LCM, PUV and datapoints.
+   *                    creates all necessary variables that is needed to check if a launch can be made
+   */
+  public Decide(Parameters parameters, LCM[][] lcm, boolean[] puv, Point[] points) {
 
-        this.parameters = parameters;
-        this.lcm = lcm;
-        this.puv = puv;
-        this.cmv = new CMV(parameters, points);
-        this.cmv.populate();
-        //this.pum = setPUM();
-        //this.fuv = setFUV();
+    this.parameters = parameters;
+    this.lcm = lcm;
+    this.puv = puv;
+    this.cmv = new CMV(parameters, points);
+    this.cmv.populate();
+    //this.pum = setPUM();
+    //this.fuv = setFUV();
 
-    }
+  }
 
-    /**
-     * Checks all conditions, and if all are met or not, returns a boolean
-     * @return a boolean, true if launch is a go, false otherwise
-     */
-    public boolean Decide(){
-        //TODO
-        //check all conditions inside here
-        return false;
-    }
+  /**
+   * Checks all conditions, and if all are met or not, returns a boolean
+   *
+   * @return a boolean, true if launch is a go, false otherwise
+   */
+  public boolean Decide() {
+    //TODO
+    //check all conditions inside here
+    return false;
+  }
 
 }
 
